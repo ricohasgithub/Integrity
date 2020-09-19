dde5d826c6c01521cb144e63f9870b414cce0c34
ricozhuthegreat <ricozhuthegreat@hotmail.com>
2020-09-19 14:39:27 -0400

    Updated main.py pyrebase

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,23 @@
 # Watch firebase for realtime updates
-from firebase import firebase
-import requests
-from firebase_admin import db
+import pyrebase
 
-f = Firebase('https://integrity-ed5a5.firebaseio.com.json')
+config = {
+  "apiKey": "AIzaSyCnhYzqx8xAE3_wZT9nUMu_xUFZdpce1Rg",
+  "authDomain": "integrity-ed5a5.firebaseapp.com",
+  "databaseURL": "https://integrity-ed5a5.firebaseio.com",
+  "storageBucket": "integrity-ed5a5.appspot.com"
+}
 
-data = requests.get(f)
+firebase = pyrebase.initialize_app(config)
+firebase_db = firebase.database()
 
+testID = "chemistrytest"
+
+def stream_handler(message):
+
+    print(message["event"]) # put
+    print(message["path"]) # /-K7yGTTEp7O549EzTYtI
+    print(message["data"]) # {'title': 'Pyrebase', "body": "etc..."}
+
+# Get the datastream from the realtime database
+data_stream = firebase_db.child("tests").child(testID).child("post").stream(stream_handler)
