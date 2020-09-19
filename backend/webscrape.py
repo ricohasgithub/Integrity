378c49d794fd2c5b02068750487bffc7054b5dbd
joeywangzr <joeywangzr@gmail.com>
2020-09-19 11:35:33 -0600

    cleaned up code

--- backend/webscrape.py
+++ backend/webscrape.py
@@ -4,27 +4,8 @@ import bs4
 forms = requests.get('https://forms.gle/37T5ybn3b4jnUtzYA')
 soup = bs4.BeautifulSoup(forms.text,'lxml')
 
-# questions = soup.find_all('div', {'class':'freebirdFormviewerComponentsQuestionBaseTitle exportItemTitle freebirdCustomFont'})
-# for i in range(len(questions)):
-#     try:
-#         print(questions[i].text)
-#     except:
-#         continue
-
-# # Get questions only
-# title = soup.find_all('div', {'class':'freebirdFormviewerViewHeaderTitle exportFormTitle freebirdCustomFont'})
-# print(title[0].text)
-
-# # Get Inputs
-# inputs = soup.find_all('div', {'class':'docssharedWizToggleLabeledPrimaryText'})
-# for i in range(len(inputs)):
-#     try:
-#         print(inputs[i].text)
-#     except:
-#         continue
 questionsNum = soup.find_all('div', {'class':'freebirdFormviewerComponentsQuestionBaseTitle exportItemTitle freebirdCustomFont'})
 
-# print(questionBox)
 for i in range(len(questionsNum)):
     questionBox = soup.find_all('div', {'class':'freebirdFormviewerViewNumberedItemContainer'})
     questionTitle = questionBox[i].find('div', {'class':'freebirdFormviewerComponentsQuestionBaseHeader'})
@@ -35,5 +16,3 @@ for i in range(len(questionsNum)):
     for z in range(len(optionsNum)):
         option = questionBox[i].find_all('div', {'class':'docssharedWizToggleLabeledPrimaryText'})
         print(option[z].text)
-# appsMaterialWizToggleRadiogroupElContainer exportContainerEl docssharedWizToggleLabeledControl freebirdThemedRadio freebirdThemedRadioDarkerDisabled
-
