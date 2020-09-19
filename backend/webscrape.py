8b265b94d6b8d91cb7cbd2852e3336a9912ea818
joeywangzr <joeywangzr@gmail.com>
2020-09-19 11:47:08 -0600

    title

--- backend/webscrape.py
+++ backend/webscrape.py
@@ -4,6 +4,9 @@ import bs4
 forms = requests.get('https://forms.gle/37T5ybn3b4jnUtzYA')
 soup = bs4.BeautifulSoup(forms.text,'lxml')
 
+testTitle = soup.find('div', {'class':'freebirdFormviewerViewHeaderTitleRow'})
+print(testTitle.text)
+
 questionsNum = soup.find_all('div', {'class':'freebirdFormviewerComponentsQuestionBaseTitle exportItemTitle freebirdCustomFont'})
 
 for i in range(len(questionsNum)):
