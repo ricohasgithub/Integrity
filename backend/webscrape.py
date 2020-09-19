cb702fc2c908487b4a6c0b4965d46c35dcb96d91
joeywangzr <joeywangzr@gmail.com>
2020-09-19 11:14:01 -0600

    update webscrape

--- backend/webscrape.py
+++ backend/webscrape.py
@@ -4,28 +4,36 @@ import bs4
 forms = requests.get('https://forms.gle/37T5ybn3b4jnUtzYA')
 soup = bs4.BeautifulSoup(forms.text,'lxml')
 
+# questions = soup.find_all('div', {'class':'freebirdFormviewerComponentsQuestionBaseTitle exportItemTitle freebirdCustomFont'})
+# for i in range(len(questions)):
+#     try:
+#         print(questions[i].text)
+#     except:
+#         continue
 
-questions = soup.find_all('div', {'class':'freebirdFormviewerComponentsQuestionBaseTitle exportItemTitle freebirdCustomFont'})
-for i in range(len(questions)):
-    try:
-        print(questions[i].text)
-    except:
-        continue
+# # Get questions only
+# title = soup.find_all('div', {'class':'freebirdFormviewerViewHeaderTitle exportFormTitle freebirdCustomFont'})
+# print(title[0].text)
 
-# Get questions only
-title = soup.find_all('div', {'class':'freebirdFormviewerViewHeaderTitle exportFormTitle freebirdCustomFont'})
-print(title[0].text)
+# # Get Inputs
+# inputs = soup.find_all('div', {'class':'docssharedWizToggleLabeledPrimaryText'})
+# for i in range(len(inputs)):
+#     try:
+#         print(inputs[i].text)
+#     except:
+#         continue
+questionsNum = soup.find_all('div', {'class':'freebirdFormviewerComponentsQuestionBaseTitle exportItemTitle freebirdCustomFont'})
 
-# Get Inputs
-inputs = soup.find_all('div', {'class':'docssharedWizToggleLabeledPrimaryText'})
-for i in range(len(inputs)):
-    try:
-        print(inputs[i].text)
-    except:
-        continue
-# for i in inputs:
-#     print ((i.attrs['name']).split(".")[1])
+# print(questionBox)
+for i in range(len(questionsNum)):
+    questionBox = soup.find_all('div', {'class':'freebirdFormviewerViewNumberedItemContainer'})
+    questionTitle = questionBox[i].find('div', {'class':'freebirdFormviewerComponentsQuestionBaseHeader'})
+    print(questionTitle.text)
 
+    optionsNum = questionBox[i].find_all('div', {'class':'docssharedWizToggleLabeledPrimaryText'})
+    
+    for z in range(len(optionsNum)):
+        option = questionBox[i].find_all('div', {'class':'docssharedWizToggleLabeledPrimaryText'})
+        print(option[z].text)
+# appsMaterialWizToggleRadiogroupElContainer exportContainerEl docssharedWizToggleLabeledControl freebirdThemedRadio freebirdThemedRadioDarkerDisabled
 
-# x = soup.get('heading')
-# print(x)
