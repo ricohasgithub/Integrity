1060fe5dacf52c4b2093c53dd79f8bcb31445179
ricozhuthegreat <ricozhuthegreat@hotmail.com>
2020-09-19 11:44:05 -0400

    Added Card Prompt Overlay

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,6 +1,10 @@
 
 document.getElementById("teacher").onclick = () => {
+
+    document.getElementById("teacher-prompt").style.visibility = "visible"; 
+
     console.log("teacher");
+
 }
 
 document.getElementById("student").onclick = () => {
