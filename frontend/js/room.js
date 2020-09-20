7e2ceeea1bc2c0fc66dda9812c3061e07877e5ad
ricozhuthegreat <ricozhuthegreat@hotmail.com>
2020-09-19 22:34:29 -0400

    Added Timer

--- frontend/js/room.js
+++ frontend/js/room.js
@@ -44,9 +44,9 @@ function parseForm (testJson) {
         let questionType = question.questionType;
         if (questionType === "Multiple Choice") {
             renderMultipleChoice(question.questionTitle, question.options);
-        } else if (questionType === "Short Answer") {
-            renderShortAnswer(question.questionTitle);
         } else if (questionType === "Written Response") {
+            renderShortAnswer(question.questionTitle);
+        } else if (questionType === "Long Answer") {
             renderLongAnswer(question.questionTitle);
         } else if (questionType === "Checkboxes") {
             renderCheckboxes(question.questionTitle, question.options);
@@ -171,6 +171,35 @@ function renderCheckboxes (question, options) {
     test.appendChild(widget);
 
 }
+
+// Timer
+let cTime = new Date();
+let nTime = new Date(cTime.getTime() + 2*60000);
+
+// Update the count down every 1 second
+let countdown = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = nTime - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("timer").innerHTML = minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("timer").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
+// Eyetracking
 
 const overlay = $('#faceoverlay')[0];
 const overlayCC = overlay.getContext('2d');
